f2ec619e59d8157e2b415812c5cebb680f599006
Manjunath <manjunath.anawal3@gmail.com>
2026-01-18 11:28:14 +0530

    Initial Commit

---
+++ BuddyAI/BuddyAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyAIApp.swift
+//  BuddyAI
+//
+//  Created by Manjunath Anawal on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct BuddyAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
