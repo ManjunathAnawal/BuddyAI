4182d83e0d5b262c7e1a2faefcb39a18ff589c94
Manjunath <manjunath.anawal3@gmail.com>
2026-01-19 23:10:32 +0530

    [feature] Add WelcomeView UI

---
+++ BuddyAI/Core/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  BuddyAI
+//
+//  Created by Manjunath Anawal on 19/01/26.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImageUrl = "https://picsum.photos/600/600"
+    static let privacyPolicyUrl = "https://www.google.com/"
+    static let termsOfServiceUrl = "https://www.google.com/"
+}
--- BuddyAI/Core/Welcome/WelcomeView.swift
+++ BuddyAI/Core/Welcome/WelcomeView.swift
@@ -8,19 +8,74 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @State var urlString = Constants.randomImageUrl
+    
     var body: some View {
         NavigationStack {
-            Text("Welcome!")
-                .frame(maxHeight: .infinity)
+            VStack(spacing: 8) {
+                imageView
+                
+                labelView
+                    .padding(.top, 24)
+                
+                ctaView
+                    .padding(10)
+                
+                policyLinks
+                    .padding(16)
+            }
+        }
+    }
+    
+    var imageView: some View {
+        ImageLoaderView(urlString: urlString)
+            .ignoresSafeArea()
+    }
+    var labelView: some View{
+        VStack(spacing: 8) {
+            Text("AI Chat 🤙🏻")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
             
+            Text("Buddy AI Chat boat")
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+        }
+    }
+    
+    var policyLinks: some View {
+        HStack {
+            Link(destination: URL(string: Constants.termsOfServiceUrl)!) {
+                Text("Terms of services")
+            }
+            
+            Circle()
+                .fill(.accent)
+                .frame(width: 4, height: 4, alignment: .center)
+            
+            Link(destination: URL(string: Constants.privacyPolicyUrl)!) {
+                Text("Privacy policy")
+            }
+            
+        }
+    }
+    var ctaView: some View {
+        VStack(spacing: 8) {
             NavigationLink {
                 OnboardingCompletedView()
             } label: {
                 Text("Get Started")
                     .callToActionButton()
             }
+            
+            Text("Alaready have an account? Sign in!")
+                .font(.body)
+                .underline()
+                .foregroundStyle(.secondary)
+                .padding(10)
+                .background(.black.opacity(0.001))
+                .onTapGesture {}
         }
-        .padding(16)
     }
 }
 
